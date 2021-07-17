f37f4d00c0b0efa135766a8ec97122506eac7e9d
Brandi Cameron <hello@brandicameron.com>
2021-07-17 14:04:25 -0500

    added console message

--- js/main.js
+++ js/main.js
@@ -170,3 +170,10 @@ fadeInElements.forEach((element) => {
 
 const currentYear = new Date().getFullYear();
 document.getElementById("copyright-year").textContent = currentYear;
+
+// ---------------- CONSOLE MESSAGE ----------------
+
+console.log(
+  "%cCrafted with ♥️ by Brandi / brandicameron.com",
+  "background:white; color: rebeccapurple; font-size:11px; padding:10px; border-radius: 20px;"
+);
